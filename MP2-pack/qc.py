695fe384c11f8869864619900a15adf3558ffca4
ist186392 <ist186392@tecnico.ulisboa.pt>
2021-11-05 16:40:41 +0000

    pc dies

--- MP2-pack/qc.py
+++ MP2-pack/qc.py
@@ -12,36 +12,6 @@ from nltk.corpus import stopwords
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 
-# #removing the stopwords
-# def remove_stopwords(text, is_lower_case=False):
-#     tokens = tokenizer.tokenize(text)
-#     tokens = [token.strip() for token in tokens]
-#     if is_lower_case:
-#         filtered_tokens = [token for token in tokens if token not in stopword_list]
-#     else:
-#         filtered_tokens = [token for token in tokens if token.lower() not in stopword_list]
-#     filtered_text = ' '.join(filtered_tokens)
-#     return filtered_text
-
-# # Bag of words without words that arent stop words in english
-# def create_bag_of_words_for_each_category(file_text):
-#     file_by_lines = file_text.splitlines()
-#     stop_words = set(stopwords.words('english'))
-#     bag_of_words_category = {}
-#     for line in file_by_lines:
-#         tokens = nltk.word_tokenize(line)
-#         category = tokens[0]
-#         filtered_tokens = [token for token in tokens[1:] if token.lower() not in stop_words]
-#         try:
-#             new_list = bag_of_words_category[category] + filtered_tokens
-#             bag_of_words_category[category] = new_list
-#         except KeyError:
-#             lista = [] + filtered_tokens
-#             bag_of_words_category[category] = lista
-#     return bag_of_words_category
-
-
-
 # Splits file in string in dict {category - sentence }
 # Removes stop words https://www.kaggle.com/lakshmi25npathi/sentiment-analysis-of-imdb-movie-reviews 
 # Removes punctuation https://www.kite.com/python/answers/how-to-remove-all-punctuation-marks-with-nltk-in-python  token.isalnum() 
@@ -80,40 +50,44 @@ def preprocessing(file_name):
 # Cada categoria é um "documento" do tf-idf https://medium.com/@paritosh_30025/natural-language-processing-text-data-vectorization-af2520529cf7
 # Usar os scores the ITDF  https://towardsdatascience.com/natural-language-processing-feature-engineering-using-tf-idf-e8b9d00e7e76
 # train = [ ( dict(unique_word1=tf_idf_score,...), sentence[key] ), () ... ]
+
 def word2vec(sentence):
     vectorizer = TfidfVectorizer()
-    
-    #From lists of lists to each category has a list
-    text = dict()
-    for key in sentence.keys():
-        text[key] = []
-        for elem in sentence[key]:
-            if len(elem)!=1:
-                for el in elem:
-                    text[key].append(el)
-            else:
-                text[key].append(elem)
 
+    print("\n\n")
     print(sentence.keys())
-    listaL = ' '.join(text["HISTORY"])
-    listaH = ' '.join(text["LITERATURE"])
+    print("\n\n")
 
-    # TODO MUSIC IS NOT WORKING
-    # print(text["MUSIC"])
-    # listaM = ' '.join(text["MUSIC"])
-    listaS = ' '.join(text["SCIENCE"])
-    listaG = ' '.join(text["GEOGRAPHY"])
+    all_sentences = []
+    i = 0
+    indexes = dict()
+    for key in sentence.keys():
+        for elem in sentence[key]:
+            all_sentences.append(' '.join(elem))
+            i+=1
+        indexes[key] = i
+    
+    #Index for knowing when each category starts
+    print(indexes)
+    print("\n\n")
+    print(all_sentences[0:2])
+    print("\n\n")
+
 
     #Tokens vectorization according to the tf idfs scores
-    vectors = vectorizer.fit_transform([listaL, listaH, listaS, listaG])
+    vectors = vectorizer.fit_transform(all_sentences)
     feature_names = vectorizer.get_feature_names_out()
     dense = vectors.todense()
-    denselist = dense.tolist()
-    df = pd.DataFrame(denselist, columns=feature_names)
     
-    #Conversion to file, not needed, just for visualization
-    # compression_opts = dict(method='zip',archive_name='out.csv')  
-    # df.to_csv('out.zip', index=False,compression=compression_opts)  
+    #BUGS HERE
+    denselist = dense.tolist()
+    print("\nndone\n")
+    df = pd.DataFrame(denselist, columns=feature_names)
+    print("\nndone\n")
+    # Conversion to file, not needed, just for visualization
+    compression_opts = dict(method='zip',archive_name='out.csv')  
+    print("\nndone\n")
+    df.to_csv('out.zip', index=False,compression=compression_opts)  
 
     #IT-DFs scores
     # vectorizer = TfidfVectorizer()
@@ -145,12 +119,12 @@ if __name__ == "__main__":
     
     #Preprocess
     dict_category_sentence_train = preprocessing(train_file)
-    dict_category_sentence_test  = preprocessing(test_file)
+    #dict_category_sentence_test  = preprocessing(test_file)
     # print(dict_category_sentence_train["LITERATURE"])
     
     #Word2Vec problem since some sentences might be larger thus the shape is not the same for all
     train_vec = word2vec(dict_category_sentence_train)
-    test  = word2vec(dict_category_sentence_test)
+    #test  = word2vec(dict_category_sentence_test)
     
     #Train
 
